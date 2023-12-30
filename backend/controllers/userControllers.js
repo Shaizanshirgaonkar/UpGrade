410387ae38b5b410b203e03e271ade8707119bbd
shaizan <shaizanshirgaonkar786@gmail.com>
2023-12-30 23:10:51 +0530

    commit

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -46,7 +46,12 @@ const registerUser = asyncHandler(async (req, res) => {
 //  @route POST a/api/users/logout
 //  @access Private
 const logoutUser = asyncHandler(async (req, res) => {
-  res.send("logout user");
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: true,
+    expires: new Date(0),
+  });
+  res.status(200).json({ message: "Logged out successfully" });
 });
 
 //  @desc Logout userprofile
