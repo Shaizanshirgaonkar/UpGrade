eaa21343154d0dccc189eab6137343d431b047b1
shaizan <shaizanshirgaonkar786@gmail.com>
2023-12-31 19:38:28 +0530

    commit

--- frontend/src/slices/usersApiSlice.js
+++ frontend/src/slices/usersApiSlice.js
@@ -3,7 +3,7 @@ import { apiSlice } from "./apiSlice";
 
 export const usersApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
-    getProducts: builder.mutation({
+    login: builder.mutation({
       query: (data) => ({
         url: USERS_URL / auth,
         method: "POST",
